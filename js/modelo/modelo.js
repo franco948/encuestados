c874aa6d8a87a269822bd34a6b4ea13d58afad16
franco948 <franco.palermo812@gmail.com>
2019-09-29 11:36:22 -0300

    Agregar funcionalidad que permite agregar una pregunta desde la vista del administrador

--- js/modelo/modelo.js
+++ js/modelo/modelo.js
@@ -1,7 +1,7 @@
 /*
  * Modelo
  */
-var Modelo = function() {
+var Modelo = function () {
   this.preguntas = [];
   this.ultimoId = 0;
 
@@ -11,20 +11,27 @@ var Modelo = function() {
 
 Modelo.prototype = {
   //se obtiene el id más grande asignado a una pregunta
-  obtenerUltimoId: function() {
+  obtenerUltimoId: function () {
+    var maxId = 1;
+
+    this.preguntas.forEach(pregunta => {
+      maxId = pregunta.id > maxId ? pregunta.id : maxId;
+    });
+
+    return maxId;
   },
 
   //se agrega una pregunta dado un nombre y sus respuestas
-  agregarPregunta: function(nombre, respuestas) {
+  agregarPregunta: function (nombre, respuestas) {
     var id = this.obtenerUltimoId();
     id++;
-    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    var nuevaPregunta = { 'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas };
     this.preguntas.push(nuevaPregunta);
     this.guardar();
     this.preguntaAgregada.notificar();
   },
 
   //se guardan las preguntas
-  guardar: function(){
+  guardar: function () {
   },
 };
--- js/vistas/vistaAdministrador.js
+++ js/vistas/vistaAdministrador.js
@@ -1,14 +1,14 @@
 /*
  * Vista administrador
  */
-var VistaAdministrador = function(modelo, controlador, elementos) {
+var VistaAdministrador = function (modelo, controlador, elementos) {
   this.modelo = modelo;
   this.controlador = controlador;
   this.elementos = elementos;
   var contexto = this;
 
   // suscripción de observadores
-  this.modelo.preguntaAgregada.suscribir(function() {
+  this.modelo.preguntaAgregada.suscribir(function () {
     contexto.reconstruirLista();
   });
 };
@@ -16,54 +16,68 @@ var VistaAdministrador = function(modelo, controlador, elementos) {
 
 VistaAdministrador.prototype = {
   //lista
-  inicializar: function() {
+  inicializar: function () {
     //llamar a los metodos para reconstruir la lista, configurar botones y validar formularios
+    this.reconstruirLista();
+    this.configuracionDeBotones();
     validacionDeFormulario();
   },
 
-  construirElementoPregunta: function(pregunta){
+  construirElementoPregunta: function (pregunta) {
     var contexto = this;
     var nuevoItem;
+
     //completar
     //asignar a nuevoitem un elemento li con clase "list-group-item", id "pregunta.id" y texto "pregunta.textoPregunta"
+    nuevoItem = $(document.createElement('li'));
+    nuevoItem.addClass('list-group-item');
+    nuevoItem.attr('id', pregunta.id);
+    nuevoItem.text(pregunta.textoPregunta);
+
     var interiorItem = $('.d-flex');
     var titulo = interiorItem.find('h5');
     titulo.text(pregunta.textoPregunta);
-    interiorItem.find('small').text(pregunta.cantidadPorRespuesta.map(function(resp){
+    interiorItem.find('small').text(pregunta.cantidadPorRespuesta.map(function (resp) {
       return " " + resp.textoRespuesta;
     }));
     nuevoItem.html($('.d-flex').html());
     return nuevoItem;
   },
 
-  reconstruirLista: function() {
+  reconstruirLista: function () {
     var lista = this.elementos.lista;
     lista.html('');
     var preguntas = this.modelo.preguntas;
-    for (var i=0;i<preguntas.length;++i){
+    for (var i = 0; i < preguntas.length; ++i) {
       lista.append(this.construirElementoPregunta(preguntas[i]));
     }
   },
 
-  configuracionDeBotones: function(){
+  configuracionDeBotones: function () {
     var e = this.elementos;
     var contexto = this;
 
     //asociacion de eventos a boton
-    e.botonAgregarPregunta.click(function() {
+    e.botonAgregarPregunta.click(function () {
       var value = e.pregunta.val();
       var respuestas = [];
 
-      $('[name="option[]"]').each(function() {
+      $('[name="option[]"]').each(function () {
         //completar
+        respuestas.push(
+          {
+            'textoRespuesta': $(this).val(),
+            'cantidad': 0
+          });
       })
+      
       contexto.limpiarFormulario();
       contexto.controlador.agregarPregunta(value, respuestas);
     });
     //asociar el resto de los botones a eventos
   },
 
-  limpiarFormulario: function(){
+  limpiarFormulario: function () {
     $('.form-group.answer.has-feedback.has-success').remove();
   },
 };
